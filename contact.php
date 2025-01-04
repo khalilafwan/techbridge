ce3ea027d452d659564ef1a0b6e158ede3cd1b81
Khalilul Afwan <khalilulafwan@student.unp.ac.id>
2025-01-04 08:47:30 +0700

    update

--- contact.php
+++ contact.php
@@ -176,41 +176,7 @@
 </head>
 <body>
     <!-- header -->
-    <header id="header" class="header d-flex align-items-center sticky-top">
-        <div class="container-fluid container-xl position-relative d-flex align-items-center">
-    
-          <a href="index.html" class="logo d-flex align-items-center me-auto">
-            <img src="assets/img/techbridge/logo.png" alt="">
-          </a>
-    
-          <nav id="navmenu" class="navmenu">
-            <ul>
-              <li><a href="index.html">Home</a></li>
-              <li><a href="#">Internship</a></li>
-              <li><a href="#">Learning Modules</a></li>
-              <li><a href="faq.html">FAQ</a></li>
-              <li><a href="contact.html" class="active">Contact</a></li>
-              <li><a href="about.html">About</a></li>
-              <li class="dropdown"><a href="#"><span>Hub</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                <ul>
-                  <li><a href="#">Premium Subscription</a></li>
-                  <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                    <ul>
-                      <li><a href="#">Deep Dropdown 1</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">Community Forum</a></li>
-                </ul>
-              </li>
-            </ul>
-            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-          </nav>
-    
-          <a class="btn-getstarted" href="login.php">Log In</a>
-          <a class="btn-getstarted" href="register.php">Register</a>
-    
-        </div>
-    </header>
+    <?php include("partials/header.php"); ?>
 
     <!-- Contact Form Section -->
     <div class="container">
---
+++ partials/header.php
@@ -0,0 +1,36 @@
+ <!-- header -->
+ <header id="header" class="header d-flex align-items-center sticky-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+    
+          <a href="index.html" class="logo d-flex align-items-center me-auto">
+            <img src="assets/img/techbridge/logo.png" alt="">
+          </a>
+    
+          <nav id="navmenu" class="navmenu">
+            <ul>
+              <li><a href="index.php" class="active">Home</a></li>
+              <li><a href="#">Internship</a></li>
+              <li><a href="#">Learning Modules</a></li>
+              <li><a href="faq.php">FAQ</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li><a href="about.php">About</a></li>
+              <li class="dropdown"><a href="#"><span>Hub</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Premium Subscription</a></li>
+                  <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                      <li><a href="#">Deep Dropdown 1</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Community Forum</a></li>
+                </ul>
+              </li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+    
+          <a class="btn-getstarted" href="login.php">Log In</a>
+          <a class="btn-getstarted" href="register.php">Register</a>
+    
+        </div>
+    </header>
